2ad83a3e3dcb631bc8c51ecc1815fa8055bbe4ad
Gary Murphy <compsciguy.it@gmail.com>
2017-01-16 11:10:08 +1000

    signed and unsigned integers

---
+++ src/integers.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let num:u8 = 255;
+    let neg:i8 = -128;
+    println!("{} is the maximum number for 8 bit unsigned integers.", num);
+    println!("{} is the lowest number for 8 bit signed integers.", neg);
+}
