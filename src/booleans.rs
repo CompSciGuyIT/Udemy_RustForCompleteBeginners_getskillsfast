072ec0f8c152f94ee6a687c5668cd1658106b505
Gary Murphy <compsciguy.it@gmail.com>
2017-01-16 14:52:52 +1000

    Boolean data types

---
+++ src/booleans.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let bool1 = true;
+    let bool2:bool = false;
+    if bool1 == true {
+        println!("bool1 is true.");
+    } else {
+        println!("bool1 is false.");
+    }
+    if bool2 == true {
+        println!("bool2 is true.");
+    } else {
+        println!("bool2 is false.");
+    }    
+}
